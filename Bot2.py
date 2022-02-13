d6261d92d3affd39f08c719dbb9b0b56797f5fc5
SadIQ <ismailaabubakar112@gmail.com>
2022-02-13 16:18:59 +0100

    Update Bot2.py

---
+++ Bot2.py
@@ -0,0 +1,17 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('NzIwNzc5NzExNTU4NTgyMjcy.XuK8ag.2_ITvGN5HU2Xn7vPI4vAdNfZA38')
